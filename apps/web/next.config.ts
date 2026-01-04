fe10284646bf47c13e00691981014626a20ec207
neverthe1ess <lent-guild.0j@icloud.com>
2026-01-05 00:55:12 +0900

    fix: 배포 시 문제되는 하드코딩 URL 제거

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,12 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'standalone',
   async rewrites() {
+    const API_URL =
+      process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:4000';
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:4000/:path*',
+        destination: `${API_URL}/:path*`,
       },
     ];
   },
