f971afd7d6d779ffcd6b6b82bc187c077cd3fb12
neverthe1ess <140958504+neverthe1ess@users.noreply.github.com>
2026-01-05 11:59:01 +0900

    fix: kakao 맵 api 문제

--- apps/web/hooks/useKakaoMap.ts
+++ apps/web/hooks/useKakaoMap.ts
@@ -7,7 +7,7 @@ export const useKakaoMap = (mapRef: React.RefObject<HTMLDivElement | null>) => {
 
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY}&autoload=false&libraries=services`;
+    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY}&autoload=false&libraries=services`;
     script.async = true;
 
     script.onload = () => {
