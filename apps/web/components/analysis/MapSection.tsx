b81504b44bc921758460986979c379eced92da12
장봉익 <bongsik9410@gmail.com>
2026-01-06 15:43:02 +0900

    refactor : 선택된 지역만 폴리곤 표시

--- apps/web/components/analysis/MapSection.tsx
+++ apps/web/components/analysis/MapSection.tsx
@@ -6,7 +6,7 @@ import AnalysisMap from './AnalysisMap';
 export default function MapSection() {
   return (
     <div className="relative h-full flex flex-col p-6 bg-white">
-      <div className="relative flex-1 rounded-2xl overflow-hidden border-4 border-gray-100 shadow-inner">
+      <div className="relative flex-1 rounded-2xl overflow-hidden border-1 border-gray-100 shadow-inner">
         <div className="absolute inset-0 z-0">
           <AnalysisMap />
         </div>
