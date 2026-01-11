7c93a47b70b51edd4f5157b87a22e16b1991c633
Jeongns <jhlee030313@gmail.com>
2026-01-12 01:10:50 +0900

    feat: 메인화면 디자인 수정

--- apps/web/components/DashboardShell.tsx
+++ apps/web/components/DashboardShell.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useMemo, useState } from "react";
+import { useMemo, useState, type CSSProperties } from "react";
 import { usePathname, useRouter } from "next/navigation";
 
 import { Sidebar } from "./Sidebar";
@@ -18,7 +18,14 @@ export function DashboardShell({ children }: { children: React.ReactNode }) {
   }, [pathname]);
 
   return (
-    <div className="min-h-screen bg-[#f7f7f8] flex relative">
+    <div
+      className="min-h-screen bg-[#f7f7f8] flex relative"
+      style={
+        {
+          "--sidebar-offset": isSidebarOpen ? "350px" : "80px",
+        } as CSSProperties
+      }
+    >
       <Sidebar
         activeMenu={activeMenu}
         onMenuClick={(id) => {
